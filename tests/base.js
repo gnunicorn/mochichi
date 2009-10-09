e117a5db77b3d2f96ef2c33ae12d3c312f65f1e8
Benjamin Kampmann <ben.kampmann@googlemail.com>
2009-10-09 14:14:30 +0200

    make tests reusable

---
+++ tests/base.js
@@ -0,0 +1,26 @@
+function runTest(test_script) {
+	try {
+			SimpleTest.saveScope(this);
+			test_script({ok:SimpleTest.ok,is:SimpleTest.is});
+			SimpleTest.verifyScope(this);
+			SimpleTest.ok( true, "test suite finished!");
+	} catch (err) {
+			var s = "test suite failure!\n";
+			var o = {};
+			var k = null;
+			for (k in err) {
+					// ensure unique keys?!
+					if (!o[k]) {
+							s +=  k + ": " + err[k] + "\n";
+							o[k] = err[k];
+					}
+			}
+			SimpleTest.ok ( false, s );
+	}
+}
+
+function runMultipleTests(tests) {
+  for (test in tests) {
+    runTest(test);
+  }
+}
